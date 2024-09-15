e7dd9a6d1ef4089ac9706333ab55f5021ad6767e
ncc02 <cuongtk2002@gmail.com>
2024-09-15 14:33:01 +0700

    15/09/2024 14:32

---
+++ CongNgheIOT/CodeTuan4_CuongPhucManh/NewZMod4/NewZMod4/ra_gen/vector_data.h
@@ -0,0 +1,32 @@
+/* generated vector header file - do not edit */
+#ifndef VECTOR_DATA_H
+#define VECTOR_DATA_H
+#ifdef __cplusplus
+        extern "C" {
+        #endif
+/* Number of interrupts allocated */
+#ifndef VECTOR_DATA_IRQ_COUNT
+#define VECTOR_DATA_IRQ_COUNT    (5)
+#endif
+/* ISR prototypes */
+void iic_master_rxi_isr(void);
+void iic_master_txi_isr(void);
+void iic_master_tei_isr(void);
+void iic_master_eri_isr(void);
+void r_icu_isr(void);
+
+/* Vector table allocations */
+#define VECTOR_NUMBER_IIC0_RXI ((IRQn_Type) 0) /* IIC0 RXI (Receive data full) */
+#define IIC0_RXI_IRQn          ((IRQn_Type) 0) /* IIC0 RXI (Receive data full) */
+#define VECTOR_NUMBER_IIC0_TXI ((IRQn_Type) 1) /* IIC0 TXI (Transmit data empty) */
+#define IIC0_TXI_IRQn          ((IRQn_Type) 1) /* IIC0 TXI (Transmit data empty) */
+#define VECTOR_NUMBER_IIC0_TEI ((IRQn_Type) 2) /* IIC0 TEI (Transmit end) */
+#define IIC0_TEI_IRQn          ((IRQn_Type) 2) /* IIC0 TEI (Transmit end) */
+#define VECTOR_NUMBER_IIC0_ERI ((IRQn_Type) 3) /* IIC0 ERI (Transfer error) */
+#define IIC0_ERI_IRQn          ((IRQn_Type) 3) /* IIC0 ERI (Transfer error) */
+#define VECTOR_NUMBER_ICU_IRQ4 ((IRQn_Type) 4) /* ICU IRQ4 (External pin interrupt 4) */
+#define ICU_IRQ4_IRQn          ((IRQn_Type) 4) /* ICU IRQ4 (External pin interrupt 4) */
+#ifdef __cplusplus
+        }
+        #endif
+#endif /* VECTOR_DATA_H */
